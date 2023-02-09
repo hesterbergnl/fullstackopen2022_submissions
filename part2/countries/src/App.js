3e3cbd22e4df703ae04bd7bdf2464d7dad7d5325
Nikolai Hesterberg <nikolai.hesterberg@gmail.com>
2023-02-09 11:26:33 +0100

    completed exercises 2.19-2.20.  There is an error thrown on first call to line 62.  Cannot understand how to remove it

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,38 +1,74 @@
 import { useState, useEffect } from 'react'
 import countryAPIService from './services/countryAPIService.js'
 
-const Country = ({country}) => {
-  return <div> {country.name.common} </div>
+const Country = ({country, showMethod }) => {
+  return <div>{country.name.common} <button onClick={showMethod}>Show</button></div>
 }
 
 const Language = ({keys, language}) => {
   return <li> {language} </li>
 }
 
-const CountryDetails = ({country}) => {
-  var keys = Object.keys(country.languages)
+const CountryDetails = ({country, weather, setWeather}) => {
+  useEffect(() => {
+      countryAPIService.getWeatherCity(country.capital)
+        .then(weather => {
+          setWeather(weather)
+      }).catch(error => console.log(error))
+  }, [country])
 
-  return (
-    <div>
-      <h2> {country.name.common} </h2>
-      <p> Capital: {country.capital} </p>
-      <p> Area: {country.area} </p>
-      {console.log(keys)}
-      <h3> Languages </h3>
-      <ul> {keys.map(key => <Language key={key} language={country.languages[key]}/>)} </ul>
-      {console.log(country.flags.png)}
-      <img src={country.flags.png} alt='flag'/>
-    </div>
-  )
+  if (country !== null) {
+    const keys = Object.keys(country.languages)
+
+    if (weather != null) {
+      return (
+      <div>
+        <h2> {country.name.common} </h2>
+        <p> Capital: {country.capital} </p>
+        <p> Area: {country.area} </p>
+        {console.log(keys)}
+        <h3> Languages </h3>
+        <ul> {keys.map(key => <Language key={key} language={country.languages[key]}/>)} </ul>
+        {console.log(country.flags.png)}
+        <img src={country.flags.png} alt='flag'/>
+
+        <h3> Weather for {country.capital} </h3>
+        <p> Temperature: {(Math.round((weather.main.temp - 273) * 100) / 100).toFixed(2)} C</p>
+        <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt='icon'/>
+        <p>Wind: {(Math.round((weather.wind.speed) * 100) / 100).toFixed(2)} m/s </p>
+
+      </div>
+      )
+    }
+
+    return <h2> {country.name.common} </h2>
+
+  }
 }
 
-const Countries = ({countries}) => {
-  if (countries.length == 1) {
-    return <CountryDetails country={countries[0]}/>
+const Countries = ({countries, selectedCountry, setSelectedCountry, setWeather, weather}) => {
+  const showMethod = (id) => {
+    const foundCountry = countries.find(country => country.cca2 === id)
+    console.log(foundCountry)
+    setSelectedCountry(foundCountry)
+    setWeather(null)
+  }
+
+  if (selectedCountry !== null) {
+    return <CountryDetails country={selectedCountry} setWeather={setWeather} weather={weather}/>
+  }
+
+  if (countries.length === 1) {
+    setSelectedCountry(countries[0])
+    setWeather(null)
+    // return <CountryDetails country={selectedCountry} setWeather={setWeather} weather={weather}/>
   }
 
   if (countries.length < 11) {
-    return <> {countries.map(country => <Country key={country.cca2} country={country}/>)} </>
+    if(selectedCountry != null) {
+      setSelectedCountry(null)
+    }
+    return <> {countries.map(country => <Country key={country.cca2} country={country} showMethod={() => showMethod(country.cca2)}/>)} </>
   }
 
   return <> Too many matches, specify another filter </>
@@ -46,6 +82,8 @@ const Search = ({search, updateSearch}) => {
 const App = () => {
   const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState(null)
+  const [weather, setWeather] = useState(null)
 
   useEffect(() => {
     countryAPIService.getAll()
@@ -55,7 +93,9 @@ const App = () => {
   }, [])
 
   const updateSearch = (event) => {
+    console.log("I'm updating")
     setSearch(event.target.value)
+    setSelectedCountry(null)
   }
 
   const countriesToShow = (search === '') ? countries : countries.filter(country => country.name.common.toLowerCase().includes(search.toLowerCase()))
@@ -64,7 +104,7 @@ const App = () => {
     <div>
       <Search search={search} updateSearch={updateSearch} />
       <div>
-        <Countries countries={countriesToShow} />
+        <Countries countries={countriesToShow} selectedCountry={selectedCountry} setSelectedCountry={setSelectedCountry} weather={weather} setWeather={setWeather}/>
       </div>
 
     </div>
--- part2/countries/src/services/countryAPIService.js
+++ part2/countries/src/services/countryAPIService.js
@@ -1,11 +1,19 @@
 import axios from 'axios'
 const baseUrl = 'https://restcountries.com/v3.1/'
+const baseUrlWeather = 'http://api.openweathermap.org/data/2.5/weather?q='
+
+const api_key = process.env.REACT_APP_API_KEY
 
 const getAll = () => {
     const request = axios.get(`${baseUrl}/all`)
     return request.then(response => response.data)
 }
 
-const countryAPIService = {getAll}
+const getWeatherCity = (city) => {
+    const request = axios.get(`${baseUrlWeather}${city}&appid=${api_key}`)
+    return request.then(response => response.data)
+}
+
+const countryAPIService = {getAll, getWeatherCity}
 
 export default countryAPIService
